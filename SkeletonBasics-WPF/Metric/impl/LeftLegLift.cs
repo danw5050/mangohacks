981f6e339ab5393c57080303b0212c1b163dca4b
Nick Swanson <mistereman22@gmail.com>
2018-02-03 16:42:17 -0500

    left leg lift

---
+++ SkeletonBasics-WPF/Metric/impl/LeftLegLift.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+
+namespace Microsoft.Samples.Kinect.SkeletonBasics.Metric {
+    class LeftLegLift : AngleMetric {
+        public LeftLegLift(Skeleton skeleton, double goalAngle) : base(skeleton, goalAngle) {
+        }
+
+        protected override double getAngle(SkeletonObject o) {
+            return getAngle(o.AnkleLeft, o.HipCenter, o.HipRight);
+        }
+    }
+}
