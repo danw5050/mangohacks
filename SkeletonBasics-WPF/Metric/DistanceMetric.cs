5a174bace7cae1fd85141188bb390220e3aae2df
Nick Swanson <mistereman22@gmail.com>
2018-02-03 18:13:08 -0500

    Shouldershrug

---
+++ SkeletonBasics-WPF/Metric/DistanceMetric.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+
+namespace Microsoft.Samples.Kinect.SkeletonBasics.Metric {
+    abstract class DistanceMetric : Metric {
+        double goalDistance;
+        public DistanceMetric(Skeleton skeleton, double goalDistance) : base(skeleton) {
+            this.goalDistance = goalDistance;
+        }
+
+        public override bool compare(Skeleton s) {
+            SkeletonObject o1 = new SkeletonObject(this.skeleton);
+            SkeletonObject o2 = new SkeletonObject(s);
+
+            double dist1, dist2;
+            dist1 = getDistance(o1);
+            dist2 = getDistance(o2);
+
+            return dist1 > dist2;
+        }
+
+        public abstract double getDistance(SkeletonObject o);
+
+        public override bool isGoal() {
+            SkeletonObject o = new SkeletonObject(this.skeleton);
+            double distance = getDistance(o);
+            return distance > this.goalDistance;
+        }
+    }
+}
---
+++ SkeletonBasics-WPF/Metric/impl/ShrugShoulders.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+
+namespace Microsoft.Samples.Kinect.SkeletonBasics.Metric.impl {
+    class ShrugShoulders : DistanceMetric {
+        public ShrugShoulders(Skeleton skeleton, double goalDistance) : base(skeleton, goalDistance) {
+        }
+
+        public override double getDistance(SkeletonObject o) {
+            double shoulderHeight = (o.ShoulderLeft.y + o.ShoulderRight.y) / 2;
+            double neckHeight = o.ShoulderCenter.y;
+
+            return neckHeight - shoulderHeight;
+        }
+    }
+}
